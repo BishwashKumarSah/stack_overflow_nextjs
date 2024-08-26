1627bba97dc750b2c3fed6968fbfc1de4a6b36a2
Bishwash Kumar Sah <bishwash.sah2121@gmail.com>
2024-08-26 20:04:45 +0530

    add favicon,assets,fonts and metaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,31 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "StackOverflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world.",
+  icons:{
+    icon:''
+  }
+  };
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotest = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotest",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -15,13 +39,14 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotest.variable}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
+          <h1 className="h1-bold">THis is h1</h1>
           {children}
         </body>
       </html>
