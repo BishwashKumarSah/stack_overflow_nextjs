d53f06d220550a89b6ce8b0312307e5373685ab9
Bishwash Kumar Sah <bishwash.sah2121@gmail.com>
2024-09-18 22:41:01 +0530

    fix: updated MetaData

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
       "Welcome to Stack Overflow – the premier destination for developers to ask questions, share knowledge, and find solutions. Join a global community where experts and enthusiasts collaborate to solve coding challenges and advance their skills. Whether you’re troubleshooting a bug or seeking advice on best practices, Stack Overflow connects you with the answers you need.",
     images: [
       {
-        url: "/assets/siteImages/home.png", // Image path in the public folder
+        url: `https://${process.env.NEXT_PUBLIC_HOST_URL}/assets/siteImages/home.png`, // Image path in the public folder
         width: 1200,
         height: 630,
       },
@@ -37,7 +37,11 @@ export const metadata: Metadata = {
     description:
       "Welcome to Stack Overflow – the premier destination for developers to ask questions, share knowledge, and find solutions. Join a global community where experts and enthusiasts collaborate to solve coding challenges and advance their skills. Whether you’re troubleshooting a bug or seeking advice on best practices, Stack Overflow connects you with the answers you need.",
     images: [
-      "/assets/siteImages/home.png", // Image path in the public folder
+      {
+        url: `https://${process.env.NEXT_PUBLIC_HOST_URL}/assets/siteImages/home.png`, // Image path in the public folder
+        width: 1200,
+        height: 630,
+      },
     ],
   },
 
